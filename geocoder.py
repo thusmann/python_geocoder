3306940e077003789d7961b1de02a35f8700cb7c
Thomas Husmann <thusmann@gmail.com>
2013-06-23 21:52:14 +0200

    exception handling for empty response

--- geocoder.py
+++ geocoder.py
@@ -8,18 +8,19 @@ for row in inputfile:
         row = row.rstrip()
         url = "http://nominatim.openstreetmap.org/search"
         payload = {"q":row, "format":"json", "polygon":"1", "addressdetails":"1",  "email":"thusmann@gmail.com"}
-        r = requests.get(url, params=payload)
-        print r.url
-        json = r.json()
-        
-        lat = json[0]["lat"]
-        lng = json[0]["lon"]
+        try:
+                r = requests.get(url, params=payload)
+                #print r.url
+                #print r.text
+                json = r.json()
+                lat = json[0]["lat"]
+                lng = json[0]["lon"]
+        except:
+                lat = "n/a"
+                lng = "n/a"
         
         newrow = [row, lat, lng]
         outputfile.writerow(newrow)
         
         print lat,lng
-
-
-        
 
