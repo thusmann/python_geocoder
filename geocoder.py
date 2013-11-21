a50b51a808eaf24ad6655bce0c82ee77c162fbfb
Thomas Husmann <thusmann@gmail.com>
2013-11-21 10:24:18 +0100

    Read from and write to CSV

--- geocoder.py
+++ geocoder.py
@@ -1,28 +1,47 @@
-import requests
+from datetime import datetime, timedelta
 import csv
-#from time import sleep
+import requests
+import requests_cache
 
-inputfile = open("placelist.txt")
-outputfile = csv.writer(open("geocoded-placelist.txt", "w"))
+requests_cache.install_cache(cache_name = "orte")
 
-for row in inputfile:
-        row = row.rstrip()
-        url_mapquest = "http://open.mapquestapi.com/nominatim/v1/search.php"
-        url_osm = "http://nominatim.openstreetmap.org/search"
-        payload = {"q":row, "format":"json", "polygon":"1", "addressdetails":"0", "countrycodes":"ch", "limit":"1"}
-        try:
-                r = requests.get(url_mapquest, params=payload)
-                #print r.url
-                #print r.text
-                json = r.json()
-                lat = json[0]["lat"]
-                lng = json[0]["lon"]
-        except:
-                lat = "n/a"
-                lng = "n/a"
-        
-        newrow = [row, lat, lng]
-        outputfile.writerow(newrow)
-        
-        print lat,lng
-        #sleep(1)
+# Import and export CSV
+ifile = open("twitterAccounts.csv", "rb")
+ofile = open("geocodedAccounts.csv", "wb")
+reader = csv.reader(ifile, delimiter = ";")
+writer = csv.writer(ofile, delimiter = ";")
+
+url_mapquest = "http://open.mapquestapi.com/nominatim/v1/search.php"
+url_osm = "http://nominatim.openstreetmap.org/search"
+
+rownum = 0
+for row in reader:
+    # Save header row.
+    newrow = []
+    lat = ""
+    lng = ""
+    if rownum == 0:
+        header = row
+    else:
+        colnum = 0
+        for col in row:
+            if header[colnum] == "location":
+                print "%-s: %s" % (header[colnum], col)
+                try:
+                    payload = {"q":col, "format":"json", "polygon":"1", "addressdetails":"0", "countrycodes":"ch", "limit":"1"}
+                    r = requests.get(url_mapquest, params=payload)
+                    json = r.json()
+                    lat = json[0]["lat"]
+                    lng = json[0]["lon"]
+                except:
+                    lat = "na"
+                    lng = "na"
+                newrow.append(lat)
+                newrow.append(lng)
+            newrow.append(col)
+            colnum += 1
+    writer.writerow(newrow)
+    rownum += 1
+
+ifile.close()
+ofile.close()
